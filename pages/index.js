5ecc9087a45836c03fc33ac28607e512682e5a52
Doug Moreira <dougmoreira@Dougs-MBP-486.lan>
2022-07-13 18:32:17 -0400

    postCard component ready, fetching data from GraphQLCMS

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  {title: 'React test', exerpt: 'Learn React testing '},
-  {title: 'React with Tailwind', exerpt: 'Learn React with Tailwind testing '}
-]
-
-const Home: NextPage = () => {
+const Home = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ const Home: NextPage = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) =>(
-            <PostCard post={post} key={post.title}/>
+            <PostCard post={post.node} key={post.title}/>
           ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
@@ -29,6 +25,13 @@ const Home: NextPage = () => {
       </div>
     </div>
   )
+}
+
+export async function  getStaticProps(){
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  }
 }
 
 export default Home
