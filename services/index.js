a8dfa9a5216a799696c5a00008011edf18056ed9
Doug Moreira <dougmoreira@Dougs-MacBook-Pro.local>
2022-07-15 11:29:17 -0400

    bug fixed on getCategories, getStatic paths, dinamic routinng, created some jsx

--- services/index.js
+++ services/index.js
@@ -55,7 +55,7 @@ export const getRecentPosts = async () =>{
   return result.posts;
 }
 
-export const getSimilarPosts = async () =>{
+export const getSimilarPosts = async (slug, categories) =>{
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]){
       posts(
@@ -71,7 +71,7 @@ export const getSimilarPosts = async () =>{
         }
     }
   `
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, {slug, categories})
   return result.posts;
 }
 
@@ -86,4 +86,37 @@ export const getCategories = async () =>{
   `
   const result = await request(graphqlAPI, query)
   return result.categories;
+}
+
+export const getPostsDetails = async (slug)=>{
+  const query = gql`
+  query GetPostDetails($slug: String!) {
+        post(where: {slug: $slug}){
+          author {
+            bio
+            id
+            name
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          title
+          exerpt
+          featuredImage {
+            url
+            }
+            categories {
+              name
+              slug
+              }
+          }
+          content{
+            raw
+          }
+        }
+      }`
+  const result = await request(graphqlAPI, query, {slug})
+  return result.post;
 }
