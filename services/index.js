391606ccd4f5b64efc27f837f28d5b6edaa13851
Doug Moreira <dougmoreira@Dougs-MacBook-Pro.local>
2022-07-20 10:51:42 -0400

    Comments jsx and funcntion to retrieve the comments  from graphqlccms

--- services/index.js
+++ services/index.js
@@ -130,3 +130,18 @@ export const submitComent = async (obj) =>{
 
   return result.json()
 }
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug:String!) {
+      comments(where: {post: {slug:$slug}}){
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+  return result.comments
+}
